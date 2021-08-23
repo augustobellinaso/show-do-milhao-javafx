776004fda590b0b285bf0d2306e451e01afcdce8
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-08-22 22:25:47 -0300

    criando e testando método para atualizar jogador

--- src/main/java/augustobellinaso/showdomilhao/dao/JogadorDAO.java
+++ src/main/java/augustobellinaso/showdomilhao/dao/JogadorDAO.java
@@ -30,4 +30,19 @@ public class JogadorDAO {
             return false;
         }
     }
+
+    public void atualizar(Jogador jogador) {
+        try {
+            String sql = "UPDATE jogador SET nome = ?, pontuacao = ? WHERE id = ?";
+            try(PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, jogador.getNome());
+                statement.setInt(2, jogador.getPontuacao());
+                statement.setInt(3, jogador.getId());
+                statement.executeUpdate();
+                connection.commit();
+            }
+        } catch (Exception e) {
+            LogUtil.getLogger(JogadorDAO.class).error(e.getCause().toString());
+        }
+    }
 }
--- src/main/java/augustobellinaso/showdomilhao/test/InsertDataForTesting.java
+++ src/main/java/augustobellinaso/showdomilhao/test/InsertDataForTesting.java
@@ -8,9 +8,15 @@ public class InsertDataForTesting {
     private static JogadorDAO jogadorDAO = new JogadorDAO();
 
     public static void main(String[] args) {
+//        Jogador jogador = new Jogador();
+//        jogador.setNome("Augusto");
+//        jogador.setPontuacao(2);
+//        System.out.println(jogadorDAO.adicionar(jogador));
+
         Jogador jogador = new Jogador();
-        jogador.setNome("Augusto");
-        jogador.setPontuacao(2);
-        System.out.println(jogadorDAO.adicionar(jogador));
+        jogador.setId(1);
+        jogador.setNome("Augusto Bellinaso");
+        jogador.setPontuacao(1000);
+        jogadorDAO.atualizar(jogador);
     }
 }
