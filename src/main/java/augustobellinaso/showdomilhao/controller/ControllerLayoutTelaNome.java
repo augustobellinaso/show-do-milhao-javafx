ca255a70a073a9af6c1fd9d73a9ebc8884dcc8a8
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-08-31 20:20:15 -0300

    adicionando voz do silvio santos

--- src/main/java/augustobellinaso/showdomilhao/controller/ControllerLayoutTelaNome.java
+++ src/main/java/augustobellinaso/showdomilhao/controller/ControllerLayoutTelaNome.java
@@ -1,20 +1,34 @@
 package augustobellinaso.showdomilhao.controller;
 
 import augustobellinaso.showdomilhao.application.ApplicationShowMilhao;
+import augustobellinaso.showdomilhao.application.JLayer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerLayoutTelaNome {
+public class ControllerLayoutTelaNome implements Initializable {
+
+    private static final String PARTICIPANTE = "src/main/resources/augustobellinaso/showdomilhao/songs/vamos-conhecer-agora-o-nosso-participante-voice.mp3";
 
     @FXML
     private void voltar(ActionEvent event) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/augustobellinaso/showdomilhao/view/LayoutTelaInicial.fxml"));
         pane.getStylesheets().add(getClass().getResource("/augustobellinaso/showdomilhao/css/buttonStyle.css").toExternalForm());
         ApplicationShowMilhao.changeScene(new Scene(pane, 800, 600));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        JLayer jLayer = new JLayer();
+        jLayer.tocar(new File(PARTICIPANTE));
+        jLayer.start();
     }
 }
